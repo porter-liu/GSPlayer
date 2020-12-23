985a29c9551de465ac8a183152e4970a89b397b2
Porter Liu <liuzhf@gmail.com>
2020-12-22 22:50:35 -0800

    Update AVPlayer+Extensions.swift

--- GSPlayer/Classes/Extension/AVPlayer+Extensions.swift
+++ GSPlayer/Classes/Extension/AVPlayer+Extensions.swift
@@ -7,11 +7,7 @@
 //
 
 import AVFoundation
-#if !os(macOS)
 import UIKit
-#else
-import AppKit
-#endif
 
 public extension AVPlayer {
     
@@ -27,7 +23,6 @@ public extension AVPlayer {
         return currentItem?.currentDuration ?? -1
     }
     
-    #if !os(macOS)
     var currentImage: UIImage? {
         guard
             let playerItem = currentItem,
@@ -36,19 +31,6 @@ public extension AVPlayer {
 
         return UIImage(cgImage: cgImage)
     }
-    #else
-    var currentImage: NSImage? {
-        guard
-            let playerItem = currentItem,
-            let cgImage = try? AVAssetImageGenerator(asset: playerItem.asset).copyCGImage(at: currentTime(), actualTime: nil)
-        else {
-            return nil
-        }
-        let width: CGFloat = CGFloat(cgImage.width)
-        let height: CGFloat = CGFloat(cgImage.height)
-        return NSImage(cgImage: cgImage, size: NSMakeSize(width, height))
-    }
-    #endif
     
     var playProgress: Double {
         return currentItem?.playProgress ?? -1
